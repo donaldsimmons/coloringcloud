0d602070ca45e75e989c4adf287c7041ede65ed2
Trey Simmons <yert511@gmail.com>
2020-05-12 21:02:35 -0500

    Add index view method

--- color/views.py
+++ color/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse, path
 
-# Create your views here.
+import boto3
+
+def index(request):
+    svg = boto3.resource('s3').Object('coloringcloud', 'svg_sheets/cheshire_cat.svg')
+    body = svg.get()['Body'].read().decode('utf-8')
+    return render(request, 'color/index.haml', {'svg':body})
