2e663f0b021c5ab660fce7efba29abe51de42da8
Trey Simmons <yert511@gmail.com>
2020-05-13 13:20:32 -0500

    Update scripts to build palatte dynamically

--- color/static/color/js/main.js
+++ color/static/color/js/main.js
@@ -4,14 +4,29 @@
     };
 
     function changeSelectedColor() {
-        $('.selected-color').removeClass('selected-color');
-        $(this).addClass('selected-color');
-        selectedColor = $(this).css('background-color');
+        if (!event) {
+            color = $('.selected-color').attr('id');
+            selectedColor = $('.selected-color').css('background-color');
+        } else {
+            $('.selected-color').removeClass('selected-color');
+            $(this).addClass('selected-color');
+            selectedColor = $(this).css('background-color');
+        };
     };
 
-    selectedColor = $('.selected-color').css('background-color');
+    function fillPalatte() {
+        swatches = $('.swatch')
+        for (i=0; i < swatches.length; i++) {
+            fillColor = swatches[i].id;
+            $(swatches[i]).css('background-color', fillColor);
+        };
+        changeSelectedColor();
+    }
+
     outline_areas = $('#Color').children();
 
     outline_areas.on('click', changeFillColor);
     $('.swatch').on('click', changeSelectedColor);
+
+    fillPalatte();
 })(jQuery);
