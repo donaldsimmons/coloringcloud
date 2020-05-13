34985e8207efd7b1ceb47e1afcff8c88a8da19b9
Trey Simmons <yert511@gmail.com>
2020-05-13 09:52:00 -0500

    Add javascript to change svg color on click

--- color/static/color/js/main.js
+++ color/static/color/js/main.js
@@ -1,3 +1,9 @@
-$(function() {
-    console.log('Hello');
+(function($) {
+    function colorArea() {
+        $(this).attr('fill', "#FF0000");
+    };
+
+    selectedColor = $('.selected-color');
+    outline_areas = $('#Color').children();
+    $(outline_areas).on('click', colorArea);
 })(jQuery);
