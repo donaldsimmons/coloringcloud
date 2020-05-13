417a5fe243b015d18af46b0cf709d21f70afa54d
Trey Simmons <yert511@gmail.com>
2020-05-13 11:52:13 -0500

    Add ability to change colors based on palatte

--- color/static/color/js/main.js
+++ color/static/color/js/main.js
@@ -1,9 +1,17 @@
 (function($) {
-    function colorArea() {
-        $(this).attr('fill', "#FF0000");
+    function changeFillColor() {
+        $(this).attr('fill', selectedColor);
     };
 
-    selectedColor = $('.selected-color');
+    function changeSelectedColor() {
+        $('.selected-color').removeClass('selected-color');
+        $(this).addClass('selected-color');
+        selectedColor = $(this).css('background-color');
+    };
+
+    selectedColor = $('.selected-color').css('background-color');
     outline_areas = $('#Color').children();
-    $(outline_areas).on('click', colorArea);
+
+    outline_areas.on('click', changeFillColor);
+    $('.swatch').on('click', changeSelectedColor);
 })(jQuery);
